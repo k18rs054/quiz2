73a7df0fd07ce8ee13ccac37954f3d230f43f6f2
k18rs054 <k18rs054@st.kyusan-u.ac.jp>
2020-01-10 07:15:58 +0900

    mobile backendのAPIキーを設定

---
+++ www/keys.js
@@ -0,0 +1,5 @@
+// This is a JavaScript file
+
+var appKey = "cc7af277b5bd74451aa59b70102dbc5dfe23f060ab753378447a2ed339b8610a";
+
+var clientKey = "e9c2ee6260028b19196cb1a361342249018607e8dedbb35c8f8129ee73309f09";
